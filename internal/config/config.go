7e61a3223b9eb9b5ac10182005bfe588d2c27aaf
ezhische <ezhische@gmail.com>
2025-06-27 17:33:59 +0300

    add concurrency

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,14 @@ import (
 )
 
 type Config struct {
-	APIToken string        `env:"QRATOR_X_QRATOR_AUTH,required"`
-	APIURL   string        `env:"QRATOR_API_URL" envDefault:"https://api.qrator.net/request"`
-	ClientID int           `env:"QRATOR_CLIENT_ID,required"`
-	Domains  []int         `env:"QRATOR_DOMAINS_IDS" envSeparator:","`
-	ProxyURL string        `env:"QRATOR_PROXY_URL"`
-	Timeout  time.Duration `env:"QRATOR_TIMEOUT" envDefault:"5s"`
-	Port     int           `env:"QRATOR_EXPORTER_PORT" envDefault:"9502"`
+	APIToken  string        `env:"QRATOR_X_QRATOR_AUTH,required"`
+	APIURL    string        `env:"QRATOR_API_URL" envDefault:"https://api.qrator.net/request"`
+	ClientID  int           `env:"QRATOR_CLIENT_ID,required"`
+	Domains   []int         `env:"QRATOR_DOMAINS_IDS" envSeparator:","`
+	ProxyURL  string        `env:"QRATOR_PROXY_URL"`
+	Timeout   time.Duration `env:"QRATOR_TIMEOUT" envDefault:"5s"`
+	Port      int           `env:"QRATOR_EXPORTER_PORT" envDefault:"9502"`
+	Concurent int           `env:"QRATOR_EXPORTER_CONCURENT" envDefault:"10"`
 }
 
 func ConfigFromEnv() (*Config, error) {
@@ -27,5 +28,14 @@ func ConfigFromEnv() (*Config, error) {
 }
 
 func CollectorFromConfig(config *Config, logger *logrus.Logger) (*collector.Collector, error) {
-	return collector.CollectorFromConfig(config.APIToken, config.ClientID, config.APIURL, config.Domains, config.ProxyURL, config.Timeout, logger)
+	return collector.CollectorFromConfig(
+		config.APIToken,
+		config.ClientID,
+		config.APIURL,
+		config.Domains,
+		config.ProxyURL,
+		config.Timeout,
+		logger,
+		config.Concurent,
+	)
 }
